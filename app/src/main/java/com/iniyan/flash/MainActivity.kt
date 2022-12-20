edd1dbeba0bdcabe303b664dffc1a3e9e1ffb8e1
IniyanKanmani <kiniyan002@gmail.com>
2022-12-20 15:39:33 +0530

    Flash App v2.0

--- app/src/main/java/com/iniyan/flash/MainActivity.kt
+++ app/src/main/java/com/iniyan/flash/MainActivity.kt
@@ -5,8 +5,6 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
-import android.hardware.camera2.CameraManager.TorchCallback
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,9 +16,9 @@ class MainActivity : AppCompatActivity() {
                 val manager: CameraManager =
                     getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
-                var torchCallback: TorchCallback? = null
+                var torchCallback: CameraManager.TorchCallback? = null
 
-                torchCallback = object : TorchCallback() {
+                torchCallback = object : CameraManager.TorchCallback() {
                     override fun onTorchModeChanged(cameraId: String, enabled: Boolean) {
 
                         super.onTorchModeChanged(cameraId, enabled)
